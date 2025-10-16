b71d62a6de81603aeb73ee414a618afa405b58a2
Albin92 <coder9269@gmail.com>
2025-10-16 09:04:43 +0530

    basic chatbot fixed run the app.py file

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Load dataset
-data = pd.read_csv("data.csv")
+data = pd.read_csv('data.csv')
 
 # Prepare TF-IDF vectorizer
 vectorizer = TfidfVectorizer()
