0ac037b1eb39afd740790da97b183ee9079209bb
Albin92 <coder9269@gmail.com>
2025-10-22 22:48:08 +0530

    new ui not fixed

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from api.chatbot import get_response
+from chatbot import get_response
 
 app = Flask(__name__)
 
@@ -12,3 +12,5 @@ def chat():
     user_input = request.form["msg"]
     response = get_response(user_input)
     return jsonify({"response": response})
+if __name__ == "__main__":
+    app.run(debug=True)
